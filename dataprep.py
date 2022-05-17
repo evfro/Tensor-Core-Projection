c49f967a8d9502c978c9a854ef15dd385694c67f
Evgeny Frolov <evfro@live.ru>
2022-05-17 14:20:10 +0300

    add early stopping + refactor

--- dataprep.py
+++ dataprep.py
@@ -1,13 +1,66 @@
-def transform_indices(data, users, items):
+import numpy as np
+from polara.preprocessing.dataframes import leave_one_out, reindex
+
+
+user_field = 'userid'
+item_field = 'itemid'
+position_field = 'positionid'
+
+
+def transform_indices(data, userid, itemid):
     data_index = {}
-    for entity, field in zip(['users', 'items'], [users, items]):
+    for entity, field in zip([user_field, item_field], [userid, itemid]):
         idx, idx_map = to_numeric_id(data, field)
         data_index[entity] = idx_map
         data.loc[:, field] = idx
     return data, data_index
+
 
 def to_numeric_id(data, field):
     idx_data = data[field].astype("category")
     idx = idx_data.cat.codes
     idx_map = idx_data.cat.categories.rename(field)
     return idx, idx_map
+
+
+def split_data(data, userid='userid', itemid='movieid', timeid='timestamp', time_q=0.95, seed=None):
+    timepoint = data[timeid].quantile(q=time_q, interpolation='nearest')
+    test_data_ = data.query(f'{timeid} >= @timepoint')
+    warm_users = test_data_[userid].unique()
+    train_data_ = data.query(f'{userid} not in @warm_users and {timeid} < @timepoint')
+    training, data_index = transform_indices(train_data_.copy(), userid, itemid)
+    
+    test_data = reindex(test_data_, data_index[item_field])
+    random_state = None if seed is None else np.random.RandomState(seed)
+    leave_one_out_config = dict(target=timeid, sample_top=True, random_state=random_state)
+    # final test data
+    testset_, holdout_ = leave_one_out(test_data, **leave_one_out_config)
+    testset, holdout = align_test_data(testset_, holdout_)
+    # validation data
+    testset_valid_, holdout_valid_ = leave_one_out(testset_, **leave_one_out_config)
+    testset_valid, holdout_valid = align_test_data(testset_valid_, holdout_valid_)
+    return (training, data_index), (testset_valid, holdout_valid), (testset, holdout) 
+
+
+def align_test_data(testset, holdout, userid='userid'):
+    test_users = np.intersect1d(testset.userid.unique(), holdout.userid.unique())
+    testset = testset.query(f'{userid} in @test_users').sort_values(userid)
+    holdout = holdout.query(f'{userid} in @test_users').sort_values(userid)
+    return testset, holdout
+
+
+def assign_positions(data, maxlen, userid='userid', itemid='movieid', timeid='timestamp'):
+    return (
+        data
+        .sort_values(timeid)
+        .assign(pos = lambda df:
+            df.groupby(userid)[itemid].transform(enumerate_events, maxlen)
+        )
+        .query('pos>=0')
+        .sort_values([userid, timeid])
+    )
+
+def enumerate_events(s, maxlen=None):
+    if maxlen is None:
+        maxlen = len(s)
+    return np.arange(maxlen-len(s), maxlen)
---
+++ model.py
@@ -0,0 +1,71 @@
+import numpy as np
+from hooi import sparse_hooi
+from dataprep import user_field, item_field, position_field
+
+
+def seqtf_model_build(config, data, data_description, iter_callback=None):
+    users = data_description[user_field]
+    items = data_description[item_field]
+    positions = data_description[position_field]
+
+    n_users = data_description["n_users"]
+    n_items = data_description["n_items"]
+    max_pos = data_description["n_pos"]
+    shape = (n_users, n_items, max_pos)
+
+    idx = data[[users, items, positions]].values
+    val = np.ones(idx.shape[0], dtype='f8')
+
+    return sparse_hooi(
+        idx, val, shape, config["mlrank"],
+        max_iters = config["max_iters"],
+        update_order=config["update_order"],
+        materialize_core=False,
+        growth_tol = config["growth_tol"],
+        seed = config["seed"],
+        iter_callback=iter_callback,
+        verbose=True,
+    )
+
+
+def tf_scoring(params, data, data_description, core_projected=True):
+    user_factors, item_factors, pos_factors, core = params
+    if core_projected:
+        assert isinstance(core, (tuple, list))
+        assert len(core[0]) == user_factors.shape[1]
+    else:
+        core = None
+
+    userid = data_description[user_field]
+    itemid = data_description[item_field]
+    posid = data_description[position_field]
+
+    tset_data = data.sort_values([userid, posid])
+    useridx = tset_data[userid].values
+    itemidx = tset_data[itemid].values
+    indptr, = np.where(np.diff(useridx, prepend=-1, append=-1))
+    scores = user_scoring(indptr, itemidx, item_factors, pos_factors, core)
+    return scores
+
+def user_scoring(indptr, indices, item_factors, pos_factors, core):
+    sequences = np.array_split(indices, indptr[1:-1])
+    n_items = item_factors.shape[0]
+    scores = np.zeros((len(sequences), n_items))
+    for u, seq in enumerate(sequences):
+        scores[u] = sequences_score(seq, item_factors, pos_factors, core)
+    return scores
+
+def sequences_score(seq, item_factors, pos_factors, core):
+    n_pos, _ = pos_factors.shape
+    user_profile = item_factors[seq[-(n_pos-1):], :]
+    seq_length, _ = user_profile.shape
+    compressed = user_profile.T @ pos_factors[-(seq_length+1):-1]
+    if isinstance(core, (tuple, list)):
+        core_factors = core[1].T
+        compressed = np.reshape(
+            core_factors @ (core_factors.T @ compressed.ravel(order='F')),
+            compressed.shape,
+            order='F'
+        )
+    scores = item_factors @ (compressed @ pos_factors[-1, :])
+    return scores
